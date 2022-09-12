786d947977a313cd3a649da92cfcac6dbfcd4bc8
Brotherscodes <dylanbrothers@hotmail.com>
2022-09-11 22:43:17 -0400

    Changes map logic.js

--- static/js/logic_step_2.js
+++ static/js/logic_step_2.js
@@ -59,40 +59,40 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [30, 30],
   zoom: 2,
-  layers: [streets]
+  layers: [navigationDark]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
+// //  Add a marker to the map for Los Angeles, California.
+// let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
 
-// Add GeoJSON data.
-let sanFranAirport = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      properties: {
-        id: "3469",
-        name: "San Francisco International Airport",
-        city: "San Francisco",
-        country: "United States",
-        faa: "SFO",
-        icao: "KSFO",
-        alt: "13",
-        "tz-offset": "-8",
-        dst: "A",
-        tz: "America/Los_Angeles",
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [-122.375, 37.61899948120117],
-      },
-    },
-  ],
-};
+// // Add GeoJSON data.
+// let sanFranAirport = {
+//   type: "FeatureCollection",
+//   features: [
+//     {
+//       type: "Feature",
+//       properties: {
+//         id: "3469",
+//         name: "San Francisco International Airport",
+//         city: "San Francisco",
+//         country: "United States",
+//         faa: "SFO",
+//         icao: "KSFO",
+//         alt: "13",
+//         "tz-offset": "-8",
+//         dst: "A",
+//         tz: "America/Los_Angeles",
+//       },
+//       geometry: {
+//         type: "Point",
+//         coordinates: [-122.375, 37.61899948120117],
+//       },
+//     },
+//   ],
+// };
 
 // // Grabbing our GeoJSON data.
 // L.geoJSON(sanFranAirport,{
@@ -102,28 +102,65 @@ let sanFranAirport = {
 //   }
 // }).addTo(map);
 
-// OnEachFeature:
-L.geoJSON(sanFranAirport, {
-  onEachFeature: function (feature, layer) {
-    layer.bindPopup(
-      "<h4>" +
-      feature.properties.city +
-      "</h4>" +
-      "<hr>" +
-      "<h5>Faa Code:</h5>" +
-      feature.properties.faa
-    );
-  },
-}).addTo(map);
+// // OnEachFeature:
+// L.geoJSON(sanFranAirport, {
+//   onEachFeature: function (feature, layer) {
+//     layer.bindPopup(
+//       "<h4>" +
+//       feature.properties.city +
+//       "</h4>" +
+//       "<hr>" +
+//       "<h5>Faa Code:</h5>" +
+//       feature.properties.faa
+//     );
+//   },
+// }).addTo(map);
 
-// link to airport geoJSON url FROM MY GITHUB:
+// link to earthquake data fro the USGS site:
+let earthquakeData =
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-let airportData =
-  "https://raw.githubusercontent.com/Brotherscodes/Mapping_Earthquakes/main/Assignment_Resources/majorAirports.json";
-
-// Grabbing our GeoJSON data. using .then method to allow file to load:
-d3.json(airportData).then(function (data) {
-  console.log(data);
+// Retrieve the earthquake GeoJSON data.
+d3.json(earthquakeData).then(function (data) {
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  // L.geoJSON(data).addTo(map);
+
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. We pass the magnitude of the earthquake into a function
+  // to calculate the radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+
+    // We turn each feature into a circleMarker on the map.
+
+    pointToLayer: function (feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo
+  }).addTo(map);
 });
+
+
