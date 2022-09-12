fd16b349b00ee50da9bb4888dbbee604c06536ad
Brotherscodes <dylanbrothers@hotmail.com>
2022-09-11 22:03:28 -0400

    changes made to index.html and logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,25 +2,71 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([40.7, -94.5], 4);
-
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
+// let map = L.map("mapid").setView([40.7, -94.5], 4);
 
 // Reference for future styling: https://docs.mapbox.com/api/maps/styles/
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer(
   "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
-    attribution:
-      'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
     accessToken: API_KEY,
-  }
-);
+  });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: "mapbox/dark-v10",
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: API_KEY,
+});
+
+// Create a street/satellite view of the map for user selection:
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: "mapbox/satellite-streets-v11",
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: API_KEY,
+});
+
+// Create another map style for user selection:
+let navigationDark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: "mapbox/navigation-night-v1",
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: API_KEY,
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark,
+  Satellite: satelliteStreets,
+  Navigation: navigationDark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
 
 // Add GeoJSON data.
 let sanFranAirport = {
@@ -48,7 +94,6 @@ let sanFranAirport = {
   ],
 };
 
-
 // // Grabbing our GeoJSON data.
 // L.geoJSON(sanFranAirport,{
 //   pointToLayer:function(feature,latlng){
@@ -59,7 +104,26 @@ let sanFranAirport = {
 
 // OnEachFeature:
 L.geoJSON(sanFranAirport, {
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h4>" + feature.properties.city + "</h4>" + "<hr>" + "<h5>Faa Code:</h5>" + feature.properties.faa);
-   }
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup(
+      "<h4>" +
+      feature.properties.city +
+      "</h4>" +
+      "<hr>" +
+      "<h5>Faa Code:</h5>" +
+      feature.properties.faa
+    );
+  },
 }).addTo(map);
+
+// link to airport geoJSON url FROM MY GITHUB:
+
+let airportData =
+  "https://raw.githubusercontent.com/Brotherscodes/Mapping_Earthquakes/main/Assignment_Resources/majorAirports.json";
+
+// Grabbing our GeoJSON data. using .then method to allow file to load:
+d3.json(airportData).then(function (data) {
+  console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
